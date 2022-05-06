fc8d4ce62762f2ac8ef1cebc5efa1016cb97c40c
Karan Risbud <risbudkaran@gmail.com>
2022-05-06 18:34:20 -0500

    Updated heirarchy

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import SignUp from './components/SignUp.js';
 import SignIn from './components/SignIn.js';
 import Welcome from './components/Welcome.js';
 
-import Home from './Home.js';
-import Tutor from './Tutor.js';
-import Profile from './Profile.js';
-import Favourites from './Favourites.js';
-import About from './About.js'
-import Appointments from './Appointments.js'
+import Home from './components/Home.js';
+import Tutor from './components/Tutor.js';
+import Profile from './components/Profile.js';
+import Favourites from './components/Favourites.js';
+import About from './components/About.js'
+import Appointments from './components/Appointments.js'
 
 import React, { Component } from 'react';
 import {Routes,Route} from 'react-router-dom'
@@ -25,7 +25,6 @@ class App extends Component {
             <Route path='/signin' element = {<SignIn />}></Route>
             <Route path='/welcome' element = {<Welcome />}></Route>
             <Route path='/home' element = {<Home />}></Route>
-            <Route path='/' element = {<Home />}></Route>
             <Route path='/profile' element = {<Profile />}></Route>
             <Route path='/favourites' element = {<Favourites />}></Route>
             <Route path='/about' element = {<About />}></Route>
